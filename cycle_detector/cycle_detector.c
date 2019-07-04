0cd26c29ea52b85fa500221fdf8042f7c055a22a
Morozov Ivan <john117.morozov@yandex.ru>
2019-07-04 20:23:13 +0400

    init

---
+++ cycle_detector/cycle_detector.c
@@ -0,0 +1,32 @@
+
+typedef struct     s_list
+{
+    int            data;
+    struct s_list  *next;
+}                  t_list;
+
+/*
+ * Алгоритм проверки наличия зацикленного односвязного списка
+ * Алгоритм "заяц и черепаха"
+ * Если связанный список зациклен - то будет момент когда
+ * указатель "заяц" будет равен указателю "черепаха", собственно такое равенство
+ * означает что связанный список зациклен.
+ */
+int        cycle_detector(const t_list *list)
+{
+    t_list *hare;
+    t_list *turtle;
+
+    if (!list)
+        return (0);
+    hare = (t_list*)list;
+    turtle = (t_list*)list;
+    while (hare && turtle && turtle->next)
+    {
+        hare = hare->next;
+        turtle = turtle->next->next;
+        if (turtle == hare)
+            return (1);
+    }
+    return (0);
+}
